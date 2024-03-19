fbffaf1143f885fa1dc16a916582aabec917840e
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-03-19 17:48:18 -0400

    using plink2 format

--- pipemake_utils/model_inds.py
+++ pipemake_utils/model_inds.py
@@ -42,7 +42,7 @@ def main():
         # Loop through the model and write the individuals to a file
         for inds in model.ind_dict.values():
             for ind in inds:
-                ind_file.write(f"{ind}\t{ind}\n")
+                ind_file.write(f"{ind}\n")
                 logging.info(f"Added {ind} to {map_args['out_prefix']}.ind.txt")
 
 if __name__ == '__main__':
