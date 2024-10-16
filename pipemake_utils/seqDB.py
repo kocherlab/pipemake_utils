fd7383bdb99332062e6b56dd1fcad600fa75ab1b
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-10-16 13:55:19 -0400

    update to class, create longest transcript script

--- pipemake_utils/seqDB.py
+++ pipemake_utils/seqDB.py
@@ -1,10 +1,8 @@
 import re
 import logging
-import argparse
 
 from Bio import SeqIO
 from collections import defaultdict
-
 
 def sniffSeqFile(input_filename, file_format="fasta", limit=10):
     def sniff(seq):
@@ -21,6 +19,8 @@ def sniffSeqFile(input_filename, file_format="fasta", limit=10):
             raise ValueError("Multiple sequence types found")
 
         return seq_matches[0]
+    
+    logging.info(f"Sniffing the sequence file: {input_filename}")
 
     # Create dict to store sniffs
     sniff_dict = defaultdict(int)
@@ -35,7 +35,10 @@ def sniffSeqFile(input_filename, file_format="fasta", limit=10):
     if not sniff_dict:
         raise ValueError("No sequences found in the file")
 
-    return sorted(sniff_dict.items(), key=lambda item: item[1], reverse=True)[0][0]
+    # Assign the sequence type
+    seq_type = max(sniff_dict, key=sniff_dict.get)
+
+    return seq_type
 
 
 class DBFileReader:
@@ -56,28 +59,11 @@ class DBFileReader:
         self.limit_attributes = limit_attributes
 
     def __iter__(self):
-        if self.type == "dna":
-            return self._readDNA()
-        elif self.type == "protein":
-            return self._readAAs()
-
-    @classmethod
-    def read(cls, database, input_filename, file_type, file_format="fasta"):
-        return cls(database, input_filename, file_type, file_format)
-
-    @classmethod
-    def readNCBI(cls, input_filename, file_type, file_format="fasta"):
-        return cls("ncbi", input_filename, file_type, file_format)
-
-    @classmethod
-    def readFlybase(cls, input_filename, file_type, file_format="fasta"):
-        return cls("flybase", input_filename, file_type, file_format)
-
-    def _readDNA(self):
         # Parse the fasta file
         for record in SeqIO.parse(self.input_filename, self.file_format):
+
             # Check if record is divisible by 3
-            if len(record.seq) % 3 != 0:
+            if self.type == 'cds' and len(record.seq) % 3 != 0:
                 logging.warning(f"{record.id} is not divisible by 3")
                 continue
 
@@ -88,6 +74,7 @@ class DBFileReader:
 
             # Update the record id
             record.id = record_attributes[self.primary_id]
+            record.name = record_attributes[self.primary_id]
 
             # Update the record description
             record.description = self._attributeStr(
@@ -96,23 +83,17 @@ class DBFileReader:
 
             yield record
 
-    def _readAAs(self):
-        # Parse the fasta file
-        for record in SeqIO.parse(self.input_filename, self.file_format):
-            # Get the record attributes
-            record_attributes = self._parseAttributes(
-                record.description, self.limit_attributes
-            )
+    @classmethod
+    def read(cls, database, input_filename, file_type, file_format="fasta", **kwargs):
+        return cls(database, input_filename, file_type, file_format, **kwargs)
 
-            # Update the record id
-            record.id = record_attributes[self.primary_id]
+    @classmethod
+    def readNCBI(cls, input_filename, file_type, file_format="fasta", **kwargs):
+        return cls("ncbi", input_filename, file_type, file_format, **kwargs)
 
-            # Update the record description
-            record.description = self._attributeStr(
-                record_attributes, skip_attributes=[self.primary_id]
-            )
-
-            yield record
+    @classmethod
+    def readFlybase(cls, input_filename, file_type, file_format="fasta", **kwargs):
+        return cls("flybase", input_filename, file_type, file_format, **kwargs)
 
     def _parseAttributes(self, *args, **kwargs):
         # Parse NCBI database attributes
@@ -142,16 +123,6 @@ class DBFileReader:
 
             # Update the record_attributes dictionary
             record_attributes[attribute_dict[0]] = attribute_dict[1]
-
-        return record_attributes
-
-    @staticmethod
-    def _parsePipemakeAttributes(record_description, limit_attributes=[]):
-        record_attributes = {}
-
-        # Assign the gene and protein_id attributes
-        record_attributes["gene"] = record_description.split("-")[0]
-        record_attributes["protein_id"] = record_description
 
         return record_attributes
 
@@ -200,6 +171,16 @@ class DBFileReader:
         return record_attributes
 
     @staticmethod
+    def _parsePipemakeAttributes(record_description, limit_attributes=[]):
+        record_attributes = {}
+
+        # Assign the gene and protein_id attributes
+        record_attributes["gene"] = record_description.split("-")[0]
+        record_attributes["protein_id"] = record_description
+
+        return record_attributes
+
+    @staticmethod
     def _attributeStr(record_attributes, skip_attributes=[]):
         attribute_str = ""
 
@@ -217,84 +198,3 @@ class DBFileReader:
             attribute_str += "[%s=%s]" % (_k, _v)
 
         return attribute_str
-
-
-def databaseParser():
-    db_parser = argparse.ArgumentParser(description="Process NCBI fasta files")
-
-    # Input and output arguments
-    db_parser.add_argument(
-        "--database", help="Source database for the input file", required=True
-    )
-    db_parser.add_argument("--input-filename", help="Input filename", required=True)
-    db_parser.add_argument("--output-prefix", help="Output prefix", required=True)
-
-    # Optional arguments
-    db_parser.add_argument(
-        "--no-cds", help="Don't output CDS sequences", action="store_true"
-    )
-    db_parser.add_argument("--input-type", help="Input type (e.g. DNA, AA)")
-    db_parser.add_argument("--input-format", help="Input format", default="fasta")
-    db_parser.add_argument("--output-format", help="Output format", default="fasta")
-    db_parser.add_argument(
-        "--output-primary-id", help="Output primary id", default="gene"
-    )
-    db_parser.add_argument(
-        "--log-filename", help="Log filename", default="process_ncbi_cds.log"
-    )
-
-    return vars(db_parser.parse_args())
-
-
-def main():
-    # Assign the arguments from the command-line
-    db_args = databaseParser()
-
-    # Set the logging configuration
-    logging.basicConfig(
-        filename=db_args["log_filename"],
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-
-    # Check if the input type is provided
-    if not db_args["input_type"]:
-        # Sniff the sequence file, if not provided
-        db_args["input_type"] = sniffSeqFile(db_args["input_filename"])
-
-    # Warn if the input cannot create the CDS
-    if db_args["input_type"] == "protein" and not db_args["no_cds"]:
-        logging.warning("Protein sequences cannot be converted to CDS")
-        db_args["no_cds"] = True
-
-    # Open files to store the processed CDS and protein sequences
-    protein_file = open(f"{db_args['output_prefix']}_aa.fa", "w")
-    if not db_args["no_cds"]:
-        cds_file = open(f"{db_args['output_prefix']}_cds.fa", "w")
-
-    for record in DBFileReader.read(
-        db_args["database"],
-        db_args["input_filename"],
-        file_type=db_args["input_type"],
-        file_format=db_args["input_format"],
-    ):
-        # Write the record to the cds file, if not no_cds
-        if not db_args["no_cds"]:
-            SeqIO.write(record, cds_file, db_args["output_format"])
-
-        # Translate the DNA record
-        if db_args["input_type"] == "dna":
-            record.seq = record.seq.translate()
-
-        # Write the record to the protein file
-        SeqIO.write(record, protein_file, db_args["output_format"])
-
-    # Close the files
-    protein_file.close()
-    if not db_args["no_cds"]:
-        cds_file.close()
-
-
-if __name__ == "__main__":
-    main()
