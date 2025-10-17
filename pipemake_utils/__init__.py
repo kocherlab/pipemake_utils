793a629d02f79fde91a1ab26bfc5a8e2acc34ce1
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2025-10-17 16:16:11 -0400

    round pve

--- pipemake_utils/__init__.py
+++ pipemake_utils/__init__.py
@@ -1,6 +1,6 @@
 # Basic Information
 __name__ = "pipemake_utils"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __summary__ = "Pipemake Utilities"
 __url__ = "https://github.com/kocherlab/pipemake_utils"
 __code__ = "https://github.com/kocherlab/pipemake_utils"
--- pipemake_utils/calc_pve.py
+++ pipemake_utils/calc_pve.py
@@ -23,6 +23,9 @@ def main():
         # Calculate PVE for GEMMA
         input_dataframe['pve'] = (2 * (input_dataframe['beta']**2 * input_dataframe['af'] * (1 - input_dataframe['af']))) / (2 * input_dataframe['beta'] * input_dataframe['af'] * (1 - input_dataframe['af']) + input_dataframe['se']**2 * 2 * 1000 * input_dataframe['af'] * (1 - input_dataframe['af']))
 
+    # Round PVE values to 7 decimal places
+    input_dataframe['pve'] = input_dataframe['pve'].round(7)
+
     # Save the results to output file
     input_dataframe.to_csv(pve_args['output_filename'], sep="\t", index=False)
 
