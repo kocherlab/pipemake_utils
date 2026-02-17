069ffca4ac2f81df790187a749370ddd919a1ee8
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2026-02-17 18:02:10 -0500

    create init version

--- pipemake_utils/label_tree.py
+++ pipemake_utils/label_tree.py
@@ -1,6 +1,12 @@
-from ete4 import Tree
+import os
+import logging
+import argparse
 
+from ete4 import Tree
 from collections import defaultdict
+
+from pipemake_utils.misc import confirmDir, confirmFile
+from pipemake_utils.logger import startLogger, logArgDict
 
 def returnDuplicateSpecies (tree):
     species_list = [leaf.split('-')[0] for leaf in tree.leaf_names()]
@@ -108,6 +114,8 @@ def confirmTree (tree):
 def createLabelDict (species_tree, species_label_symbols='[]'):
     label_dict = defaultdict(list)
 
+    logging.info("Creating label dictionary from species tree")
+
     for node in species_tree.traverse():
         if not node.is_leaf and node.name:
             node_children = []
@@ -121,51 +129,109 @@ def createLabelDict (species_tree, species_label_symbols='[]'):
             if node.name[0] in species_label_symbols and node.name[-1] in species_label_symbols:
                 node.name = node.name[1:-1]
             elif node.name[0] in species_label_symbols or node.name[-1] in species_label_symbols:
-                print(f"Warning: Node name '{node.name}' has mismatched label symbols.")
+                logging.warning(f"Node name '{node.name}' has mismatched label symbols.")
             
             label_dict[node.name] = node_children
+
+            logging.info(f"Added label '{node.name}' with species: {node_children}")
+    
+    logging.info(f"Label dictionary created with {len(label_dict)} labels.")
 
     return dict(sorted(label_dict.items(), key=lambda x: sum(len(sub) for sub in x[1])))
 
 def analysisLabelFile (label_filename):
-    analysis_labels = {}
-    #with open(label_filename, 'r') as label_file:
-    for label_str in label_filename.splitlines():
-        if not label_str.strip():
-            continue
-        if ':' not in label_str:
-            raise IOError(f"Warning: Line '{label_str}' does not contain a ':' character. Please check the format of the label file.")
-        for analysis_label_data in label_str.split(';'):
-            analysis_label, analysis_nodes_str = analysis_label_data.split(':')
-            for analysis_node in analysis_nodes_str.split(','):
-                analysis_labels[analysis_node.strip()] = analysis_label.strip()
+
+    logging.info(f"Reading analysis label file from {label_filename}")
+    
+    # Create a dictionary to hold the analysis labels
+    analysis_labels = defaultdict(lambda: defaultdict(str))
+
+    # Read the analysis label file and create a dictionary mapping node labels to analysis labels
+    with open(label_filename, 'r') as label_file:
+        for label_str in label_file:
+
+            # Skip empty lines
+            if not label_str.strip():
+                continue
+
+            # Raise an error if the line does not contain a ':' character
+            if ':' not in label_str:
+                raise IOError(f"Warning: Line '{label_str}' does not contain a ':' character. Please check the format of the label file.")
+            
+            # Assign the analysis name, and data
+            analysis_name, analysis_data = label_str.split()
+
+            logging.info(f"Processing analysis label: {analysis_name.strip()}")
+
+            # Process the analysis label data
+            for analysis_label_data in analysis_data.split(';'):
+                analysis_label, analysis_nodes_str = analysis_label_data.split(':')
+                for analysis_node in analysis_nodes_str.split(','):
+                    analysis_labels[analysis_name.strip()][analysis_node.strip()]= analysis_label.strip()
+
+                    logging.info(f"Mapping node '{analysis_node.strip()}' to label '{analysis_label.strip()}'")
+
+    logging.info(f"Analysis label dictionary created with {len(analysis_labels)} analyses.")
+
     return analysis_labels
 
-species_str = '((((1,2)[A],3)[B],4)[C],(5,6)[D])[F];'
+def treeParser ():
+    tree_parser = argparse.ArgumentParser(description='Label gene trees based on a species tree and an analysis label file')
+    tree_parser.add_argument('--species-tree', help = 'Newick string of the species tree with node labels', type=str, required = True, action = confirmFile())
+    tree_parser.add_argument('--analysis-label-file', help = 'String of the analysis label file in the format "ANALYSIS_LABEL:NODE1,NODE2;ANALYSIS_LABEL2:NODE3,NODE4"', type=str, required = True, action = confirmFile())
+    tree_parser.add_argument('--gene-tree-dir', help = 'Directory containing the gene trees to be labelled', type=str, required = True, action = confirmDir())
+    tree_parser.add_argument('--output-dir', help = 'Output directory for the labelled gene trees', type=str, default = 'labelled_trees')
+    return vars(tree_parser.parse_args())
 
-species_tree = Tree(species_str, parser=1)
+def main ():
+    # Assign the arguments from the command-line
+    tree_args = treeParser()
 
-node_label_dict = createLabelDict(species_tree)
+    # Start the logger and log the arguments
+    startLogger(f'{tree_args["output_dir"]}.log')
+    logArgDict(tree_args)
 
-label_file = 'TEST:A,B,C,1;REF:D\n'
-analysis_label_dict = analysisLabelFile(label_file)
+    # Read in the species tree
+    species_tree = Tree(tree_args['species_tree'], parser=1)
+    logging.info(f"Species tree read from {tree_args['species_tree']}")
 
+    # Create a dictionary mapping node labels to the corresponding groups of species
+    node_label_dict = createLabelDict(species_tree)
 
-def testTree (tree_str):
-    tree = Tree(tree_str, parser=1)
-    if not confirmTree(tree):
-        return
-    labeled_tree = labelTreeNodes(tree, node_label_dict, analysis_label_dict)
-    print(labeled_tree.to_str(props=['name'], compact=True))
+    # Create the analysis label dictionary from the analysis label file
+    analysis_labels = analysisLabelFile(tree_args['analysis_label_file'])
+    
+    # Loop the analysis labels and label the gene trees for each analysis
+    for analysis_name, analysis_label_dict in analysis_labels.items():
 
-example_str = '((((1-1,2-1),3-1),4-1),(5-1,6-1));'
-testTree(example_str)
+        logging.info(f"Labelling gene trees for analysis '{analysis_name}'")
 
-example_str = '((((1-1,1-2),3-1),4-1),(5-1,6-1));'
-testTree(example_str)
+        # Loop the gene trees in the specified directory
+        for gene_tree_file in os.listdir(tree_args['gene_tree_dir']):
 
-#example_str = '((((1-1,8-1),3-1),4-1),(5-1,6-1));'
-#testTree(example_str)
+            # Skip files that do not end with '.tre'
+            if not gene_tree_file.endswith('.tre'):
+                continue
 
-#example_str = '((((1-1,2-1),1-2),4-1),(5-1,6-1));'
-#testTree(example_str)
+            # Read in the gene tree
+            gene_tree = Tree(os.path.join(tree_args['gene_tree_dir'], gene_tree_file), parser=1)
+
+            # Confirm that the gene tree is valid and can be labelled
+            if not confirmTree(gene_tree):
+                logging.error(f"Tree '{gene_tree_file}' failed the confirmation check. Skipping this tree.")
+                continue
+
+            # Label the gene tree
+            labelled_gene_tree = labelTreeNodes(gene_tree, node_label_dict, analysis_label_dict)
+
+            # Create the output directory for the labelled gene trees if it does not exist
+            output_dir = os.path.join(tree_args['output_dir'], analysis_name)
+            os.makedirs(output_dir, exist_ok=True)
+
+            # Write the labelled gene tree to the output directory
+            labelled_gene_tree.write(outfile=os.path.join(output_dir, gene_tree_file), parser=1)
+
+            logging.info(f"Labelled tree for '{gene_tree_file}'")
+
+if __name__ == '__main__':
+    main()
